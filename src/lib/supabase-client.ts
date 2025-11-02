56b95b810359c52ae1c9b9897a6786eaa4e003d3
ryantmedeiros-1130 <ryant.medeiros@gmail.com>
2025-11-02 01:09:57 -0400

    fix: Replace createBrowserClient with createClient from @supabase/supabase-js

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,7 +1,7 @@
-﻿import { createBrowserClient } from "@supabase/supabase-js";
+﻿import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseBrowser(jwt?: string) {
-  return createBrowserClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     jwt
